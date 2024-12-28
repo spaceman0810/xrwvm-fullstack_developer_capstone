eccf58cee88abf71b635617bad767c089c2b4f04
spaceman0810 <neerajnaga@outlook.com>
2024-12-28 12:09:04 -0500

    Adding temporary changes to Github 22122024 Module 4

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -9,20 +10,21 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(
         CarMake, on_delete=models.CASCADE, related_name="models"
     )  # Many-to-One relationship
     name = models.CharField(max_length=100)
-    
+
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+
     year = models.IntegerField(
         default=2023,
         validators=[MaxValueValidator(2023), MinValueValidator(2015)],
