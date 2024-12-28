be69939ba96e8a07f9cdafdfb25c486bcccceff9
spaceman0810 <146539163+spaceman0810@users.noreply.github.com>
2024-12-28 21:16:40 +0000

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -47,10 +47,10 @@ def initiate():
     for data in car_model_data:
         try:
             car_model_instance = CarModel.objects.create(
-                name=data['name'], 
-                car_make=data['car_make'], 
-                type=data['type'], 
-                year=data['year'], 
+                name=data['name'],
+                car_make=data['car_make'],
+                type=data['type'],
+                year=data['year'],
                 dealer_id=data['dealer_id']
             )
             logger.info(f"Created CarModel: {data['name']} - {data['car_make'].name}")
